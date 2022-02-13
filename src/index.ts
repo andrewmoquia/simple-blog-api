12e46e69644cdaf886cdbe356956cb3a6c84d2ad
andrewmoquia <andrewmoquia09@gmail.com>
2022-02-14 02:33:04 +0800

    Change class name from Express to App

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Express } from './model/express';
+import { App } from './services/app';
 
 //Create express app.
-const app = new Express();
+const app = new App();
 
 //Start the app.
 app.start();
