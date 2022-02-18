f247b56975e8f08b16c9af52aea14449a18edffb
andrewmoquia <andrewmoquia09@gmail.com>
2022-02-18 10:45:55 +0800

    Updated app.ts

--- src/services/app.ts
+++ src/services/app.ts
@@ -22,7 +22,7 @@ export class App {
 
    public start() {
       //
-      //Connect to MongoDB
+      //Connect to MongoDB.
       this.database.connect();
       //
       //Allow test in localhost:3000.
@@ -36,26 +36,23 @@ export class App {
          })
       );
       //
-      //Recognize the incoming Request Object as a JSON Object
+      //Recognize the incoming Request Object as a JSON Object.
       this.app.use(express.json());
       //
-      //Parse application/x-www-form-urlencoded, basically can only parse incoming Request Object if strings or arrays
-      this.app.use(express.urlencoded({ extended: true }));
+      //Parse application/form-data.
+      this.app.use(express.urlencoded({ extended: false }));
       //
       //Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
       this.app.use(cookieParser(this.cookieSecret));
       //
-      //Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
-      // this.app.use(bodyParser.raw());
-      //
-      //Add 11 layer of security
+      //Add 11 layer of security.
       //https://helmetjs.github.io/
       this.app.use(helmet());
       //
       //Handle routes for blog requests.
       this.app.use(BlogRouter);
       //
-      //Initiate the server app
+      //Initiate the server app.
       this.app.listen(this.port, () => {
          console.log('Server is up.');
       });
